a8c04027c0520fa8a9e1b5b903426f223f4eace1
MoisesGA <mgonzalezy@intellego.com.mx>
2019-10-29 10:03:28 -0600

    Second test commit

--- angular-eight-app/src/app/test/test.component.ts
+++ angular-eight-app/src/app/test/test.component.ts
@@ -2,25 +2,42 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-test',
-  template: `<h2>
+  template: `<h2 class="text-success">
             {{name}}
           </h2>
-
-          <h2>{{2+2}}</h2>
-          <h2>{{"Welcome: "  + name}}</h2>
-          <h2>{{"Welcome: "  + name.length +" size"}}</h2>
-          <h2>{{"Uppercase: " + name.toUpperCase()}}</h2>
-          <h2>{{greetUser()}}</h2>
-          <h2>{{siteUrl}}</h2>
-
+          <input  [id]="myId" type="text" value="Maveware">
+          <input  bind-disabled="isDisable" id="{{myId}}" type="text" value="Maveware">
+          <h2 [class]="successClass">This is bingding class</h2>
+          <h2 [class]="successClass" class="text-special">This is bingding class</h2>
+          <h2 [class.text-danger]="hasError">This is bingding class</h2>
+          <h2 [ngClass]="messageClasses">This is bingding for messageClasses</h2>
   `,
-  styles: [` div{
-            color:red;
-  }`]
+  styles: [` .text-success {
+                color:green;
+              }
+              .text-danger {
+                color:red;
+              }
+              .text-special {
+                font-style:italic;
+              }
+  
+  
+  `]
 })
 export class TestComponent implements OnInit {
   public name = 'Maveware';
   public siteUrl = window.location.href;
+  public myId="testId";
+  public isDisable = false;
+  public successClass = "text-success";
+  public hasError = true;
+  public isSpecial = true;
+  public messageClasses = {
+      "text-success": !this.hasError,
+      "text-danger":this.hasError,
+      "text-special": this.isSpecial
+  }
   
   constructor() { }
 
