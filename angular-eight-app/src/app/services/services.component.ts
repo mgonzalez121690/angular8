0cd1a20527b93783045533560afc73238040fcf4
MoisesGA <mgonzalezy@intellego.com.mx>
2019-11-14 19:04:12 -0600

    Se agrega projecto routing

--- angular-eight-app/src/app/services/services.component.ts
+++ angular-eight-app/src/app/services/services.component.ts
@@ -5,6 +5,7 @@ import { EmployeeService } from '../employee.service';
   selector: 'app-services',
   template:  `
               <h2> Employee List </h2>
+              <h3>{{errorMsg}}</h3>
               <ul *ngFor="let employee of employees ">
                 <li>{{employee.name}} </li>
               </ul>  
@@ -14,14 +15,15 @@ import { EmployeeService } from '../employee.service';
 export class ServicesComponent implements OnInit {
 
   public employees =  [];
+  public errorMsg = '';
   constructor(private _employeeService:EmployeeService) { 
 
   }
 
   ngOnInit() {
     this._employeeService.getEmployees()
-        .subscribe(data => this.employees = data);
-        //.subscribe( data => this.employees = data );
+        .subscribe(data => this.employees = data,                  
+                  error => this.errorMsg = error.status);
   }
 
 }
