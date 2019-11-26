98fe47569dd5e58a0a908bb0698a677c96907cab
MoisesGA <mgonzalezy@intellego.com.mx>
2019-11-25 19:30:10 -0600

    Se agrega modulo para server expression

--- angular-forms/src/app/app.component.ts
+++ angular-forms/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { User } from './user';
 import { TestBed } from '@angular/core/testing';
+import { EnrollmentService } from './enrollment.service'; 
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,26 @@ import { TestBed } from '@angular/core/testing';
 })
 export class AppComponent {
   public topics = ['Angular','React','Vue'];
-  public userModel = new User('Roob','rob@test.com',5555564879, 'React','morning',true);
+  public userModel = new User('Rob','rob@test.com',5555564879, 'default','morning',true);
+  public topicHasError = true;
+
+  constructor(private enrollmentService: EnrollmentService){}
+
+  validateTopic(value){
+      if( value === 'default'){
+          this.topicHasError = true;
+      } else{
+        this.topicHasError = false;
+      }
+  }
+
+  onSubmit(){
+    console.log(this.userModel);
+    this.enrollmentService.enroll(this.userModel)
+        .subscribe( 
+          data => console.log('success', data),
+          error => console.error('Error!', error)
+         )
+    
+  }
 }
